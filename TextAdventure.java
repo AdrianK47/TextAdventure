7cef001e2b4e6fc29a9e8d2aef87eeca110b980d
AdrianK47 <114188316+AdrianK47@users.noreply.github.com>
2024-10-20 16:00:51 -0400

    ehhehehe

--- TextAdventure.java
+++ TextAdventure.java
@@ -27,13 +27,15 @@ public class TextAdventure
 
     // Change ourHero's name
     // ADD CODE HERE
-    
-    // describe the starting situation. Feel free to change this
-    System.out.println("You wake up to find yourself on the edge of a shadowy forest with the sun nearly set. \nYou see what looks like a city in the distance. \nWhat would you like to do? \ncity: go towards the city\nforest: turn around and re-enter the forest\nnap: go back to sleep\n" + ourHero.getName() + ": ");
+    ourHero = new Player(input, 100, 0);
 
+
+    // describe the starting situation. Feel free to change this
+    System.out.println("Long ago, two races rules over Earth: HUMANS and MONSTERS. \nOne day, war broke out between the two races. \nAfter a long battle, the humans were victorious. \nThey sealed the monsters underground with a magic spell. \nMany years later... \nLegends say that those who climb the mountain never return.\n You, a human wandered into the mountain and tripped into a hole, falling all the way down.");
     // get user input and go to the appropriate zone based on their input
     // ADD CODE HERE
-
+    System.out.println("You wake up in a room with light shining from a hole at the celing of the room. In front of you, there is an gate with a omnious presence looming outward. \n" + "What would you like to do? \n" + "continue: go forward into the dark \\n" + "nap: go back to sleep and give up escaping\n" + ourHero.getName() + ":");
+    input = inScanner.nextLine();
   }
 
   private void enterZone1()
