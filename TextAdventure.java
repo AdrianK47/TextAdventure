8834214391ac4c137cd161a64c188fc3a4712d16
AdrianK47 <114188316+AdrianK47@users.noreply.github.com>
2024-10-22 21:58:37 -0400

    heheheheh

--- TextAdventure.java
+++ TextAdventure.java
@@ -150,24 +150,26 @@ public class TextAdventure
         System.out.println("What do you want to do? \nAttack: Strike the enemy. \nDefend: Block this turn's attack from the enemy \nRun Away: Leave the battle in the worst way possible..\n" + ourHero.getName() + ": ");
         input = inScanner.nextLine();
         if (input.equals("Attack") || input.equals("attack")){
-          floweyHealth -= damage;
+          floweyHealth -= 10;
         }
         else if(input.equals("Defend") || input.equals("defend")){
           System.out.println("You defend against incoming attacks for this turn.");
         }
         else{
-          //Game over sign
+          gameOver();
         }
-        damage = (int)((Math.random()*10) + 1);
         ourHero.setHealth(ourHero.getHealth() - damage);
         System.out.println("Ferenc the flower's health is at " + floweyHealth);
         System.out.println("Ferenc the flower dealt " + damage + ". Your Current Health is now " + ourHero.getHealth() + ".");
       }
       console.setImage("normalflower.png");
       ourHero.setHealth(100);
+      Thread.sleep(3 * 1000);
       System.out.println("After dealing the last blow, Ferenc the flower reverted back into a normal yellow flower.");
+      Thread.sleep(3 * 1000);
       System.out.println("You successfully defeated Ferenc the flower!");
       // Game ending 2 where you stay with the chill dude.
+      gameEnd2();
     }
   }
 
@@ -203,103 +205,190 @@ public class TextAdventure
       System.out.println("What do you want to do? \nAttack: Strike the enemy. \nDefend: Block this turn's attack from the enemy \nRun Away: Leave the battle in the worst way possible..\n" + ourHero.getName() + ": ");
         input = inScanner.nextLine();
         if (input.equals("Attack") || input.equals("attack")){
-          lucaHealth -= damage;
+          lucaHealth -= 10;
         }
         else if(input.equals("Defend") || input.equals("defend")){
           System.out.println("You defend against incoming attacks for this turn.");
         }
         else{
-          //Game over sign
+          gameOver();
         }
-        damage = (int)((Math.random()*10) + 1);
         ourHero.setHealth(ourHero.getHealth() - damage);
         System.out.println("The GREAT Luca's health is at " + lucaHealth);
         System.out.println("The GREAT Luca dealt " + damage + ". Your Current Health is now " + ourHero.getHealth() + ".");
     }
     ourHero.setHealth(100);
+    Thread.sleep(3 * 1000);
     System.out.println("Blast!!! I cannot believe that me, THE GREAT LUCA, lost to a human. \n Welp, well played young human!!!!");
+    Thread.sleep(3 * 1000);
     System.out.println("You defeated the GREAT and MIGHTY Luca!!!");
     enterZone4();
   }
 
-  private void enterZone4()
+  private void enterZone4() throws InterruptedException
   {
+    String input;
     // change image
     // ADD CODE HERE
     console.setImage("hallway2.png");
-    
-
+    Thread.sleep(3 * 1000);
+    console.setImage("blackscree.png");
+    System.out.println("You continue down the hallway into a dark and narrow bridge with the moon glaring into your eyes");
     // describe the area/situation to the user. 
     // Give them options for choices.
     // ADD CODE HERE
+    System.out.println("What would you like to do? \nContinue: Keep going into the abyss. \nNap: Take a rest, or even get a sweet surprise" + ourHero.getName() + ": ");
+    input = inScanner.nextLine();
 
     // Take action or go to another zone based on their choice
     // ADD CODE HERE
-    
+    if (input.equals("Continue") || input.equals("continue")){
+      enterZone5();
+    }
+    else{
+      ourHero.setHealth(1000000);
+    }
   }
 
-  private void enterZone5()
+  private void enterZone5() throws InterruptedException
   {
+    String input;
     // change image
     // ADD CODE HERE
-
+    console.setImage("lucasansnormal.png");
+    System.out.println("As you progress down the path, a short, skinny man blocks your path.");
+    Thread.sleep(3000);
+    System.out.println("He says, hey you killed my brother, the GREAT and MIGHTY Luca!");
+    Thread.sleep(3000);
+    console.setImage("lucasans.png");
+    System.out.println("You'll pay for that!");
     // describe the area/situation to the user. 
     // Give them options for choices.
     // ADD CODE HERE
-
+    int sansHealth = 100;
+    while (sansHealth != 0 || sansHealth > 0){
+      int damage = (int)((Math.random()*10) + 1);
+      System.out.println("What do you want to do? \nAttack: Strike the enemy. \nDefend: Block this turn's attack from the enemy \nRun Away: Leave the battle in the worst way possible..\n" + ourHero.getName() + ": ");
+        input = inScanner.nextLine();
+        if (input.equals("Attack") || input.equals("attack")){
+          sansHealth -= 10;
+        }
+        else if(input.equals("Defend") || input.equals("defend")){
+          System.out.println("You defend against incoming attacks for this turn.");
+        }
+        else{
+          gameOver();
+        }
+        ourHero.setHealth(ourHero.getHealth() - damage);
+        System.out.println("???'s health is at " + sansHealth);
+        System.out.println("??? dealt " + damage + ". Your Current Health is now " + ourHero.getHealth() + ".");
+    }
+    ourHero.setHealth(100);
+    System.out.println("The person blocking your path, silently disintegrates");
+    Thread.sleep(3 * 1000);
+    System.out.println("You successfully defeated ???!!");
+    enterZone6();
     // Take action or go to another zone based on their choice
     // ADD CODE HERE
     
   }
 
-  private void enterZone6()
+  private void enterZone6() throws InterruptedException
   {
+    String input;
     // change image
     // ADD CODE HERE
-
+    System.out.println("You sense your journey's end is nearing...");
+    Thread.sleep(3000);
+    System.out.println("An overwhelming presence starts to make you shiver.");
+    console.setImage("madgortgore.png");
+    System.out.println("I am Gort, King of the uhhhhhh..... Flowers and People.");
+    Thread.sleep(3000);
+    console.setImage("gortgore.png");
+    System.out.println("I need to defeat you to free my people.");
     // describe the area/situation to the user. 
     // Give them options for choices.
     // ADD CODE HERE
-
+    int gortHealth = 100;
+    while (gortHealth != 0 || gortHealth > 0){
+      int damage = (int)((Math.random()*10) + 1);
+      System.out.println("What do you want to do? \nAttack: Strike the enemy. \nDefend: Block this turn's attack from the enemy \nRun Away: Leave the battle in the worst way possible..\n" + ourHero.getName() + ": ");
+        input = inScanner.nextLine();
+        if (input.equals("Attack") || input.equals("attack")){
+          gortHealth -= 10;
+        }
+        else if(input.equals("Defend") || input.equals("defend")){
+          System.out.println("You defend against incoming attacks for this turn.");
+        }
+        else{
+          gameOver();
+        }
+        ourHero.setHealth(ourHero.getHealth() - damage);
+        System.out.println("???'s health is at " + gortHealth);
+        System.out.println("??? dealt " + damage + ". Your Current Health is now " + ourHero.getHealth() + ".");
+    }
+    System.out.println("You.... defeated me.");
+    Thread.sleep(3000);
+    System.out.println("You defeated Gort, the King of Monster???");
     // Take action or go to another zone based on their choice
     // ADD CODE HERE
-    
+    gameEnd3();
   }
 
-  private void gameEnd()
+  private void gameEnd() throws InterruptedException
   {
     // ADD CODE HERE
 
     // Add the first ending picture
+    console.setImage("ending1.png");
     System.out.println("You give up escaping and continue to cry for help...");
+    Thread.sleep(3000);
     System.out.println("...");
+    Thread.sleep(3000);
     System.out.println("...");
+    Thread.sleep(3000);
     System.out.println("...");
-
+    Thread.sleep(3000);
     inScanner.close();
   }
-  private void gameEnd2()
+  private void gameEnd2() throws InterruptedException
   {
     // ADD CODE HERE
 
-    // Add the first ending picture
-    System.out.println("You give up escaping and continue to cry for help..."); // change to ending 2's description.
+    // Add the second ending picture
+    console.setImage("ending2.png");
+    System.out.println("You chill with Ian while listening to sick beats."); // change to ending 2's description.
+    Thread.sleep(3000);
     System.out.println("...");
+    Thread.sleep(3000);
     System.out.println("...");
+    Thread.sleep(3000);
     System.out.println("...");
+    Thread.sleep(3000);
 
     inScanner.close();
   }
-  private void gameEnd3()
+  private void gameEnd3() throws InterruptedException
   {
     // ADD CODE HERE
 
-    // Add the first ending picture
-    System.out.println("You give up escaping and continue to cry for help..."); // change to ending 3's description.
+    // Add the third ending picture
+    console.setImage("ending3.png");
+    System.out.println("You finally escape but at what cost??..."); // change to ending 3's description.
+    Thread.sleep(3000);
     System.out.println("...");
+    Thread.sleep(3000);
     System.out.println("...");
+    Thread.sleep(3000);
     System.out.println("...");
+    Thread.sleep(3000);
 
+    inScanner.close();
+  }
+  private void gameOver() throws InterruptedException
+  {
+    console.setImage("gameover.png");
+    Thread.sleep(5000);
     inScanner.close();
   }
 }
